df19ce0fd9cdf4d3c0770e2112983fd7cc6cf218
unknown <max.a.lewis98@gmail.com>
2022-02-20 12:36:39 -0700

    created basic files and chessboard

---
+++ app.js
@@ -0,0 +1,31 @@
+var chessBoard = document.getElementById("chessBoard");
+
+$("#testButton").on("click", function () {
+    console.log("test")
+})
+
+function clickBoard(cell) {
+    console.log("click")
+    console.log(cell)
+}
+
+$(document).ready(() => {
+
+    for (var i = 0; i < 8; i++) {
+        row = chessBoard.insertRow(i);
+        for (var j = 0; j < 8; j++) {
+            cell = row.insertCell(j);
+            cell.onclick = function () { clickBoard(this) };
+            cell.setAttribute("row", i);
+            cell.setAttribute("cell", j);
+            cell.classList.add("box", "border");
+        }
+    }
+
+    console.log(chessBoard)
+
+    var testcell = chessBoard.rows[1].cells[3]
+
+    console.log(testcell);
+
+});
